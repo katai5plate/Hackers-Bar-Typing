dfa50ab1ef8606d2b145abd76dfb6a31768b1c00
katai5plate <katai5plate.sub@gmail.com>
2018-01-10 01:24:26 +0900

    5 letters shortened

--- Hackers_Bar.js
+++ Hackers_Bar.js
@@ -1,5 +1,5 @@
 //ChromeでF12押すと出てくるコンソールにぶち込めばおｋ
 
 const d=document,e=d.createEvent("Events"),x=u=>u.charCodeAt(0);
-"HACKERS BAR".split("").forEach((v,i)=>{e.initEvent("keydown",!0,!0);
-e.which=e.keyCode=x(v);e.shiftKey=!i||v=="B";d.dispatchEvent(e);})
+"HACKERS BAR".split("").forEach((v,i)=>{e.initEvent("keydown",1,1);
+e.which=e.keyCode=x(v);e.shiftKey=!i|i==8;d.dispatchEvent(e);})
